f158569b2ea58c328f70d5e20a7864b1ee55dfa3
Caetano Burjack <caetano.burjack@gmail.com>
2022-05-05 17:23:34 -0300

    finishing some configs

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
+import AppLoading from 'expo-app-loading';
+import { View } from 'react-native';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Widget } from './src/components/Widget';
 import { theme } from './src/theme';
+import Widget from './src/components/Widget';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, Inter_500Medium
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={{ flex: 1, backgroundColor: theme.colors.background }}>
-      <Text>Bem Vindo meu amigo!</Text>
-
-      <Widget />
-
       <StatusBar
         style="light"
         backgroundColor='transparent'
         translucent
       />
+
+      <Widget />
     </View>
   );
-};
+}
--- README.md
+++ README.md
@@ -11,6 +11,11 @@ expo init // start configurations
 
 expo install expo-font @expo-google-fonts/inter
 
+expo install expo-app-loading
+
+npm i --save phosphor-react-native
+
+expo install react-native-svg
 
 
 ```
